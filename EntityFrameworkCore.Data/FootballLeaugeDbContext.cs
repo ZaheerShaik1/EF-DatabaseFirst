1ab912b853a9588c03616529e3d099c5ec19fa32
ZaheerShaik1 <shaik.zaheer@veltris.com>
2024-11-04 13:25:59 +0530

    Entity framework database first approach

---
+++ EntityFrameworkCore.Data/FootballLeaugeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Data
+{
+
+    
+    public class FootballLeaugeDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeauge_EfCore");
+        }
+        public DbSet<Team> Teams { get; set; }  
+        public DbSet<League> Leauges { get; set; }
+    }
+}
---
+++ EntityFrameworkCore.Domain/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Domain
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int LeagueId { get; set; }
+
+        public virtual League League { get; set; }
+    }
+}
